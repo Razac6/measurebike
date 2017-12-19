7604de1ae336d56ea7ae32317e04d9ea7b3a1307
marcin <binekmarcin@gmail.com>
2017-12-19 21:32:06 +0100

    refactor

---
+++ src/app/share/services/strava.service.spec.ts
@@ -0,0 +1,15 @@
+import {inject, TestBed} from '@angular/core/testing';
+
+import {StravaService} from './strava.service';
+
+describe('StravaService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [StravaService]
+    });
+  });
+
+  it('should be created', inject([StravaService], (service: StravaService) => {
+    expect(service).toBeTruthy();
+  }));
+});
